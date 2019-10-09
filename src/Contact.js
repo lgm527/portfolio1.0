170fc0328c5f64fe00cfcf4d686353199193d069
lgm527 <laurellmccaffrey@gmail.com>
2019-10-09 14:31:10 -0400

    contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
-function Contact() {
-  return (
-    <div className="Contact">
+class Contact extends React.Component {
 
-    </div>
-  );
+  state = {
+    from_name: '',
+    from_email: '',
+    message: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const templateParams = {
+     from_name: this.state.from_name,
+     from_email: this.state.from_email,
+     message: this.state.message
+    };
+    emailjs.send('default_service', 'contact_form', templateParams, 'user_930tGXbXZ8DTaedds14NL' )
+       .then((response) => {
+           console.log('SUCCESS!', response.status, response.text);
+       }, (err) => {
+           console.log('FAILED...', err);
+      });
+  }
+
+
+  render(){
+    return (
+      <div className="Contact">
+
+        <form id="contact-form" onSubmit={this.handleSubmit}>
+            <input type="hidden" name="contact" />
+            <label>Name</label>
+            <input type="text" name="from_name" id='from_name' onChange={this.handleChange} />
+            <label>Email</label>
+            <input type="email" name="from_email" id='from_email' onChange={this.handleChange} />
+            <label>Message</label>
+            <textarea name="message" id='message' onChange={this.handleChange} />
+            <input type="submit" value="Send" />
+        </form>
+
+      </div>
+    )
+  }
+
 }
 
 export default Contact;
